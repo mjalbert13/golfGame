b09eb9d34e5c93f6f0067727cffcd78876f8cfbe
mjalbert13 <mjalbert13@gmail.com>
2020-04-11 14:02:23 -0400

    stating golf sim game

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var path =  require('path')
+var router =  express.Router()
+var PORT = 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.listen(PORT, function(){
+    console.log("App is running on port "+PORT)
+});
